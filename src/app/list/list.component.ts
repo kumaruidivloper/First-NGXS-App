72641313cad70c472426e270a752fb52db8dc10b
Kumaravel Shanmugam <M1050211@mindtree.com>
2019-04-07 16:08:20 +0530

    delete-modal-imp

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
 import { UserState } from './../state/user.state';
 import { Select, Store } from '@ngxs/store';
 import { User } from './../models/user.model'
-import { Observable, empty } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { DeleteUser, GetUsers, SetSelectedUser } from './../actions/user.action';
 
 @Component({
@@ -13,10 +14,24 @@ import { DeleteUser, GetUsers, SetSelectedUser } from './../actions/user.action'
 })
 export class ListComponent {
     public gridLength: object;
+    public modalRef: BsModalRef;
+    public message: string;
+    public selectedUser: number;
+    public selectedId: number;
+
     @Select(UserState.getUserList) users: Observable<User[]>;
 
-    constructor(private store: Store, private router: Router) {
+    constructor(
+        private store: Store, 
+        private router: Router,
+        private modalService: BsModalService) {
     }
+
+    openModal(template: TemplateRef<any>, user: User) {
+        this.selectedUser = user.userId;
+        this.selectedId = user.id;
+        this.modalRef = this.modalService.show(template, {class: 'modal-md'});
+      }
 
     ngOnInit() {
         this.users.subscribe(res => {
@@ -25,9 +40,21 @@ export class ListComponent {
         this.store.dispatch(new GetUsers());
     }
 
-    deleteUser(id: number) {
+    confirm(id: number): void {
+        console.log(id);
+        this.message = 'Confirmed!';
+        this.modalRef.hide();
         this.store.dispatch(new DeleteUser(id));
-    }
+      }
+     
+      decline(): void {
+        this.message = 'Declined!';
+        this.modalRef.hide();
+      }
+
+    // deleteUser(id: number) {
+    //     this.selectedUser = id;
+    // }
 
     editUser(payload: User) {
         this.store.dispatch(new SetSelectedUser(payload));
--- src/app/list/list.module.ts
+++ src/app/list/list.module.ts
@@ -5,6 +5,7 @@ import { DetailComponent } from '../detail/detail.component';
 import { ListComponent } from '../list/list.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';  
+import { ModalModule } from 'ngx-bootstrap';
 
 const routes: Routes = [
     { path: '', component: ListComponent },
@@ -17,7 +18,8 @@ const routes: Routes = [
     imports: [
       RouterModule.forChild(routes),
       ReactiveFormsModule,
-      CommonModule
+      CommonModule,
+      ModalModule.forRoot()
     ],
     declarations: [
         FormComponent,
